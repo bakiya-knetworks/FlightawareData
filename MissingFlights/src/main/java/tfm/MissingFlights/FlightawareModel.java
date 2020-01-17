03959376787ef92a11d1dcc38d69e0946ce71bc3
bakiya-knetworks <bakiya@knetworks.co.in>
2020-01-17 21:30:16 +0530

    deleted project

--- MissingFlights/src/main/java/tfm/MissingFlights/FlightawareModel.java
+++
@@ -1,6 +0,0 @@
-package tfm.MissingFlights;
-
-public class FlightawareModel {
-	public String ident, type, origin,dest, departuretime, arrivaltime;
-	public long departuretimemilliseconds, arrivaltimemilliseconds;
-}
